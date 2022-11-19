30f8ccfe7f5e5dcd916f59f954802b058397282f
Eva Reitzel <evareitzel@gmail.com>
2022-11-18 23:20:32 -0500

    commit tree d0de4d4bf15a26e16705223e7859913f080d3c73

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :word, :match
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+
+    comparison_arr = @word.split("").sort
+
+    return arr.filter do |str|
+      sorted_arr = str.split("").sort
+
+      if(sorted_arr == comparison_arr)
+        str 
+      end
+    end
+  end
+end
+
+
+# Flatiron's solution
+
+# @name = word
+  
+  # def match(array)
+  #   array.select{|x| x.split("").sort == @name.split("").sort}
+  # end
